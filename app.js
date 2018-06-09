4ea1e68042f8ba8aab134254d84419b2e70911da
Ahmed <remmemberallah@gmail.com>
2018-06-09 12:55:24 +0200

    finishing add

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-
-module.exports =  function(site){
+module.exports = function (site) {
     const channels = require('./libs/channels')(site)
-    site.get({name : '/my-channels' , path : __dirname + '/site_files/html/index.html' , parser : "html css js"})
+    site.get({
+        name: '/my-channels',
+        path: __dirname + '/site_files/html/index.html',
+        parser: "html css js"
+    })
 
-    site.post('/api/youtube/addchannel',(req,res) => {
+    site.post('/api/youtube/addchannel', (req, res) => {
         let channel = req.body
         channel.user_id = req.session.user.id
         channel.created_date = new Date()
@@ -12,14 +15,57 @@ module.exports =  function(site){
         channel.likes_count = 0
         channel.rate = 0
         channel.dislike_count = 0
-        channels.addchannel(channel,(err,doc) => {
-            if(!err){
-                res.json({done:true})
-            }else{
-                res.json({done:false,err:err.message})
+        channels.addchannel(channel, (err, doc) => {
+            if (!err) {
+                res.json({
+                    done: true
+                })
+            } else {
+                res.json({
+                    done: false,
+                    err: err.message
+                })
             }
         })
     })
-    
+
+    site.post('/api/youtube/getUserChannels', (req, res) => {
+        let user_id = req.session.user.id
+        channels.getUserChannels(user_id, (channels) => {
+            res.json(channels)
+        })
+    })
+
+    site.post("uploadFile", (req, res) => {
+        var response = {
+            done: true
+        }
+        var file = req.files.fileToUpload
+        var newpath = __dirname + "/images/" + file.name
+        site.mv(file.path, newpath, function (err) {
+            if (err) {
+                response.error = err
+                response.done = false
+            }
+            response.FilePath = "/api/images/" + file.name
+            res.end(JSON.stringify(response))
+        })
+    })
+
+    //custom Route
+    site.get('/api/images/:name', (req, res) => {
+        site.readFile(__dirname + "/images/" + req.params.name, (err, content) => {
+            res.set('content-type', 'image/jpg')
+            res.end(content, 'binary')
+        })
+    })
+
+
+    site.on('mongodb after insert',info => {
+        console.log("data inserted")        
+    })
+
+
+
 
 }
--- libs/channels.js
+++ libs/channels.js
@@ -1,31 +1,42 @@
-module.exports = function(site){
-    let $channels = site.connectCollection({collection : "channels" , db : "videos"})
-    
+module.exports = function (site) {
+    let $channels = site.connectCollection({
+        collection: "channels",
+        db: "videos"
+    })
+
     const channels = {
-        data : []
+        data: []
     }
 
-    channels.init = function(){
-        $channels.getAll({},(err,docs) => {
-            if(!err && docs){
+    channels.init = function () {
+        $channels.getAll({}, (err, docs) => {
+            if (!err && docs) {
                 channels.data = docs
             }
         })
     }
 
-    channels.loadAll = function(){
-        console.log(channels.data )
-        return channels.data 
+    channels.loadAll = function () {
+        return channels.data
     }
 
-    channels.addchannel = function(channel,callback){
-        $channels.insert(channel,(err,doc) => {
-            callback(err,doc)
-            if(!err){
+    channels.addchannel = function (channel, callback) {
+        $channels.insert(channel, (err, doc) => {
+            callback(err, doc)
+            if (!err) {
                 channels.data.push(channel)
             }
         })
-        
+
+    }
+
+    channels.getUserChannels = function (user_id, callback) {
+
+        $channels.getAll({where: {'user_id': user_id}}, (err, docs) => {
+            if (!err) {
+                callback(docs)
+            }
+        })
     }
 
     channels.init()
